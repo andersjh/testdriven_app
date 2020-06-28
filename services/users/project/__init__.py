c83eb0d5cd07f5c3302a406e2571fe739c72ab8f
andersjh <andersjh1120@gmail.com>
2020-06-27 19:33:03 -0500

    blueprint refactor

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,38 +1,30 @@
 # services/users/project/__init__.py
 
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-api = Api(app)
+def create_app(script_info=None):
 
-# set config
-app_settings = os.getenv('APP_SETTINGS')  
-app.config.from_object(app_settings)  
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')  
+    app.config.from_object(app_settings)  
 
-# model
-class User(db.Model):  # new
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    # set up extensions
+    db.init_app(app)   
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    # shell context fro flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-api.add_resource(UsersPing, '/users/ping')        
+    return app
